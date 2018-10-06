ff28ca0855856e75a61a64c3175c17f114dc5cba
indra <indraswarna123@gmail.com>
2018-10-06 08:55:52 +0700

    init

---
+++ src/main/java/id/ac/sgu/homecontroller/App.java
@@ -0,0 +1,13 @@
+package id.ac.sgu.homecontroller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
